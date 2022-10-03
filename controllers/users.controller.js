b120d74e145c0797ccb791867f33057dfba84700
axelbriandip <axelbriandip.rg@gmail.com>
2022-10-03 04:42:10 -0300

    create user and login done

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -12,11 +12,60 @@ const { AppError } = require('../utils/appError.util');
 dotenv.config({ path: './config.env' });
 
 const createUser = catchAsync(async (req, res, next) => {
-	// block code
+	const { username, email, password, role } = req.body;
+
+	if (role !== 'admin' && role !== 'normal') {
+		return next(new AppError('Invalid role', 400));
+	}
+
+	// Encrypt the password
+	const salt = await bcrypt.genSalt(12);
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	const newUser = await User.create({
+		username,
+		email,
+		password: hashedPassword,
+		role
+	});
+
+	// Remove password from response
+	newUser.password = undefined;
+
+	// 201 -> Success and a resource has been created
+	res.status(201).json({
+		status: 'success',
+		data: { newUser },
+	});
 });
 
 const login = catchAsync(async (req, res, next) => {
-	// block code
+	// Get email and password from req.body
+	const { email, password } = req.body;
+
+	// Validate if the user exist with given email
+	const user = await User.findOne({
+		where: { email, status: 'active' },
+	});
+
+	// Compare passwords (entered password vs db password)
+	// If user doesn't exists or passwords doesn't match, send error
+	if (!user || !(await bcrypt.compare(password, user.password))) {
+		return next(new AppError('Wrong credentials', 400));
+	}
+
+	// Remove password from response
+	user.password = undefined;
+
+	// Generate JWT (payload, secretOrPrivateKey, options)
+	const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+		expiresIn: '30d',
+	});
+
+	res.status(200).json({
+		status: 'success',
+		data: { user, token },
+	});
 });
 
 const getMyProducts = catchAsync(async (req, res, next) => {
